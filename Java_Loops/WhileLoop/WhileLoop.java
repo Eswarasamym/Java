c774755b27c7cd2c8eba7823e70bf4f7fa815120
Eswarasamym <eswarasamy87@gmail.com>
2025-01-29 19:00:13 +0530

    File Added

---
+++ Java_Loops/WhileLoop/WhileLoop.java
@@ -0,0 +1,9 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int i = 1;  // Starting value
+        while (i <= 10) {  // Condition to check
+            System.out.println(i);  // Print the value of i
+            i++;  // Increment the value of i by 1
+        }
+    }
+}
