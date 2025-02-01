689f5176708f889becb90d224202ee6e8eb42c84
Eswarasamym <eswarasamy87@gmail.com>
2025-02-01 19:24:45 +0530

    File Added

---
+++ Java-Method/Static_Method/Demo.java
@@ -0,0 +1,17 @@
+
+ class Demo {
+    public static void m1()
+    {
+
+    }
+    public static double m2()
+    {
+        return 2.3;
+    }
+    public static String m3()
+    {
+        System.out.println("Bye");
+        return "java";
+    }
+    
+}
