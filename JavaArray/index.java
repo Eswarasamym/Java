7fb8c8cfe0a748e88531d0c7fb45bb1c1f41c91f
Eswarasamym <eswarasamy87@gmail.com>
2025-02-03 11:34:53 +0530

    file added

---
+++ JavaArray/index.java
@@ -0,0 +1,13 @@
+
+class index {
+    public static void main(String[] args)
+     {
+       
+        int[] numbers = {10, 20, 30, 40, 50};
+        
+        // Access and print elements
+        System.out.println("First element: " + numbers[0]);
+        System.out.println("Second element: " + numbers[1]);
+    }
+}
+
