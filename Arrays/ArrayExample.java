b24f14fe85e40343646ffbead61448f5d77e0849
Eswarasamym <eswarasamy87@gmail.com>
2025-02-08 12:23:40 +0530

    File added

---
+++ Arrays/ArrayExample.java
@@ -0,0 +1,9 @@
+class ArrayExample {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // Accessing array elements
+        System.out.println("First Element: " + numbers[0]); // Output: 10
+        System.out.println("Second Element: " + numbers[1]); // Output: 20
+    }
+}
