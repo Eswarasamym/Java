4379ae916f9b634ab37342cf368186c39b837252
Eswarasamym <eswarasamy87@gmail.com>
2025-02-09 14:21:30 +0530

    File Added

---
+++ Constructor/GenLaptopobj.java
@@ -0,0 +1,11 @@
+class GenLaptopobj {
+    public static void main(String[] args) {
+        Laptop l1=new Laptop("Dell",500000);
+        Laptop l2=new Laptop("HP",400000);
+        Laptop l3=new Laptop("Lenovo",600000);
+        l1.detailsOfLaptop();
+        l2.detailsOfLaptop();
+        l3.detailsOfLaptop();
+    }
+    
+}
