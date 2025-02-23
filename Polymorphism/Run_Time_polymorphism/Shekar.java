7f1cabc143ffd50f2ced7b9e2589d088c6e64425
Eswarasamym <eswarasamy87@gmail.com>
2025-02-23 22:01:52 +0530

    File Added

---
+++ Polymorphism/Run_Time_polymorphism/Shekar.java
@@ -0,0 +1,12 @@
+class HarshaSir{
+    public void food(){
+        System.out.println("Veg Food");
+    }
+    public void playchess(){
+        System.out.println("Playing chess");
+    }
+}
+class Shekar extends HarshaSir{
+    
+
+}
